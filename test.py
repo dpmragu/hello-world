1997fbdcced2db40104f88b5ecc3f20fecd89891
dpmragu <snpragu@hotmail.com>
2021-03-11 11:42:40 +0530

    comment added

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Pulls data from the Rails and Go databases into the DS AWS S3 bucket.
+
+Dependencies:
+psycopg2==2.8.5
+pandas==1.0.3
+pyarrow==0.17.0
+s3fs==0.4.2
+boto3==1.12.49
+
+Possible Databases: ['Go', 'Rails']
+"""
 import pd from pandas
